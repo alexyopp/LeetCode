66d60869d52d8bbf32f0939fdd8b1061cb59c529
Alex Yopp <alexyopp@gmail.com>
2026-06-16 09:06:42 -0700

    Solution to Problem 3612

---
+++ 3612 - Process String with Special Operations I/Solution1.h
@@ -0,0 +1,53 @@
+#pragma once
+
+//  For each character in string s
+//      if *, remove the last character from result (check for empty result first)
+//      if #, append result to itself (result = result + result)
+//      if %, reverse result
+//      else, append character to result
+
+#include <string>
+#include <algorithm>      // for swap
+
+using namespace std;
+
+namespace Solution1
+{
+    class Solution {
+    public:
+        string processStr(string s) {
+            string result{};
+            for (auto character : s)
+            {
+                if (character == '*')
+                {
+                    if (result.size() > 0)
+                    {
+                        result.pop_back();
+                    }
+                }
+                else if (character == '#')
+                {
+                    result = result + result;
+                }
+                else if (character == '%')
+                {
+                    auto startIter = result.begin();
+                    auto endIter = result.end() - 1;
+                    while (endIter - startIter > 0)
+                    {
+                        iter_swap(startIter, endIter);
+                        ++startIter;
+                        --endIter;
+                    }
+                }
+                else
+                {
+                    result += character;
+                }
+            }
+
+            return result;
+        }
+    };
+}
